b35c97785bc97e069f50117126f63c9249fd3de8
swissmilo <milo.spirig@gmail.com>
2025-01-27 12:18:39 -0500

    update install script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Create and activate virtual environment
-python3 -m venv venv
-source venv/bin/activate
-
 # Install required system packages
 sudo apt-get update
 sudo apt-get install -y \
@@ -12,6 +8,31 @@ sudo apt-get install -y \
     bluetooth \
     bluez \
     libbluetooth-dev
+
+# Add user to bluetooth group
+sudo usermod -a -G bluetooth $USER
+
+# Create bluetooth configuration
+sudo tee /etc/dbus-1/system.d/bluetooth.conf > /dev/null << EOL
+<?xml version="1.0" encoding="UTF-8"?>
+<busconfig>
+  <policy user="$USER">
+    <allow own="org.bluez"/>
+    <allow send_destination="org.bluez"/>
+    <allow send_interface="org.bluez.GattCharacteristic1"/>
+    <allow send_interface="org.bluez.GattDescriptor1"/>
+    <allow send_interface="org.freedesktop.DBus.ObjectManager"/>
+    <allow send_interface="org.freedesktop.DBus.Properties"/>
+  </policy>
+</busconfig>
+EOL
+
+# Restart bluetooth service
+sudo systemctl restart bluetooth
+
+# Create and activate virtual environment
+python3 -m venv venv
+source venv/bin/activate
 
 # Install Python dependencies
 pip install -r requirements.txt
